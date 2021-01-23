20caec14c49a11b1048493e957be0a187857d22d
TomasPabloFerreira <tomasferreira373@gmail.com>
2021-01-23 06:36:00 -0300

    refactor: add and remove reservations

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -16,7 +16,7 @@ const getTimeFrameStatus = (reservations: string[], userId: string) => (
 			: 0
 )
 
-const getTimeFramesStatus = (days: Day[], userId: string): Day[] => {
+const getAllTimeFramesStatus = (days: Day[], userId: string): Day[] => {
 	const newDays = days.map(x => ({
 		...x,
 		timeFrames: x.timeFrames.map(y => ({
@@ -27,6 +27,23 @@ const getTimeFramesStatus = (days: Day[], userId: string): Day[] => {
 	return newDays
 }
 
+const removeReservation = (timeFrame: TimeFrame, userId: string) => {
+	return {
+		reservations: timeFrame.reservations.filter(x => x !== userId),
+		status: getTimeFrameStatus(
+			timeFrame.reservations.filter(x => x !== userId),
+			userId
+		)
+	}
+}
+
+const addReservation = (timeFrame: TimeFrame, userId: string) => {
+	return {
+		reservations: timeFrame.reservations.concat(userId),
+		status: getTimeFrameStatus(timeFrame.reservations.concat(userId), userId)
+	}
+}
+
 const Calendar = ({ userId }: Props) => {
 	const [days, setDays] = useState(initialData.days)
 	const [timeRange] = useState(initialData.timeRange)
@@ -35,28 +52,21 @@ const Calendar = ({ userId }: Props) => {
 	const daysLabels: string[] = ['Time', ...days.map(x => x.label)]
 
 	useEffect(() => {
-		const newDays = getTimeFramesStatus(initialData.days, userId)
+		const newDays = getAllTimeFramesStatus(initialData.days, userId)
 		setDays(newDays)
 	}, [userId])
 
 	const toggleTimeFrameStatus = (dayId: string, timeFrameIndex: number) => {
-		let newDays: Day[] = [...days]
-		const dayIndex = newDays.findIndex(x => x._id === dayId)
-		const day = newDays[dayIndex]
+		const dayIndex = days.findIndex(x => x._id === dayId)
+		const day = days[dayIndex]
 		const timeFrame: TimeFrame = day.timeFrames[timeFrameIndex]
 
 		if(timeFrame.status === 2) return
 
+		let newDays: Day[] = [...days]
 		timeFrame.reservations.includes(userId)
-			? newDays[dayIndex].timeFrames[timeFrameIndex] = {
-				reservations: timeFrame.reservations.filter(x => x !== userId),
-				status: getTimeFrameStatus(timeFrame.reservations.filter(x => x !== userId), userId)
-			}
-			: newDays[dayIndex].timeFrames[timeFrameIndex] = {
-				reservations: timeFrame.reservations.concat(userId),
-				status: getTimeFrameStatus(timeFrame.reservations.concat(userId), userId)
-			}
-
+			? newDays[dayIndex].timeFrames[timeFrameIndex] = removeReservation(timeFrame, userId)
+			: newDays[dayIndex].timeFrames[timeFrameIndex] = addReservation(timeFrame, userId)
 		setDays(newDays)
 	}
 
